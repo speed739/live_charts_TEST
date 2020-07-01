9dbc82d3611760113b89090143c8eec204f92fc2
Grzesiek a <grzesiek7393223@o2.pl>
2020-07-01 09:47:54 +0200

    Dodaj pliki projektów.

---
+++ live_charts_TEST/Charts_Values.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace live_charts_TEST
+{
+    public class Charts_Values
+    {
+        public double a_value = 1;
+        public double b_value = 2;
+        public double c_value = 3;
+        public double d_value = 4;
+    
+    }
+}
---
+++ live_charts_TEST/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Helpers;
+using LiveCharts.Wpf;
+
+namespace live_charts_TEST
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double i = 0, x = 0, y = 0, z = 0;
+
+        public Func<ChartPoint, string> PointLabel { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void Change_Chart_Values()
+        {
+           
+
+        }
+        private void Chart_OnDataClick(object sender, ChartPoint chartPoint)
+        {
+            
+
+
+            //|var chart = (LiveCharts.Wpf.PieChart)chartPoint.ChartView;
+
+            ////clear selected slice.
+            //foreach (PieSeries series in chart.Series)
+            //    series.PushOut = 0;
+
+            //var selectedSeries = (PieSeries)chartPoint.SeriesView;
+            //selectedSeries.PushOut = 8;
+        }
+
+        private void btn_a_Click(object sender, RoutedEventArgs e)
+        {
+            i++;
+            List<double> list = new List<double>() {i};
+            part_blue.Values = list.AsChartValues();
+          
+        }
+        private void btn_b_Click(object sender, RoutedEventArgs e)
+        {
+            x++;
+            List<double> list = new List<double>() { x };
+            part_red.Values = list.AsChartValues();
+        }
+
+        private void btn_c_Click(object sender, RoutedEventArgs e)
+        {
+            y++;
+            List<double> list = new List<double>() { y };
+            part_yellow.Values =  list.AsChartValues();
+
+        }
+
+    }
+}
